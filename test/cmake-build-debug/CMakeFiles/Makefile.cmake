b413c7159630fa0bfcf0061864d09f6afb4084a0
夏木 <728467189@qq.com>
2020-09-27 10:02:04 +0800

    9.27

--- test/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ test/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -54,5 +54,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/test.dir/DependInfo.cmake"
-  "CMakeFiles/kk.dir/DependInfo.cmake"
+  "CMakeFiles/i.dir/DependInfo.cmake"
   )
--- test/main.cpp
+++ test/main.cpp
@@ -1,13 +1,58 @@
 #include <iostream>
-using namespace std;
-int main() {
-    cout << "Hello, World!" << endl;
-    cout << "hello ,i'am a chicken" <<endl;
-    int x=190;
-    int y = 0;
-    float s;
-    s = 1000;
-    y +=x;
+using namespace std ;
 
+typedef int Element ;
+typedef int Status;
+#define OK 0
+#define FALSE 0
+#define ERROR -1
+#define OVERFLOW -2
+#define maxsize 10
+
+typedef struct {
+    Element * elem;
+    int length;
+}sqlist;
+
+Status Init(sqlist &l)
+{
+    l.elem = new Element (maxsize);
+    if(!l.elem)return ERROR;
+    l.length = 0;
+    return OK;
+}
+
+Status Writeelem(sqlist &l ,int i){
+    if (i<1||i>maxsize)return OVERFLOW;
+    for (int j = 1; j < maxsize+2; ++j) {
+        l.elem[j] = j;
+        l.length++;
+    }
+    return OK;
+}
+
+Status Getelem(sqlist l,int i,Element &e)
+{
+    if (i<1||i>l.length) return ERROR;
+    e = l.elem[i];
+    for (int j = 0; j < l.length; ++j) {
+        if(l.elem[j]==e)
+            return j;
+    }
+    return OK;
+}
+
+Status Insertelem(sqlist &l,int i,Element &e)
+{
+    return OK;
+}
+
+int main(){
+    int e;
+    auto *l= new sqlist ();
+    Init(*l);
+    Writeelem(*l,10);
+    cout<<l->length;
+    cout<<Getelem(*l,9,e);
     return 0;
 }
