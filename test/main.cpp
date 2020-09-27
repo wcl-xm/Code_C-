8d46297c35ed7eb96634ebb469a0e08ca96449ca
夏木 <728467189@qq.com>
2020-09-27 11:27:48 +0800

    9.27

--- test/main.cpp
+++ test/main.cpp
@@ -4,7 +4,6 @@ using namespace std ;
 typedef int Element ;
 typedef int Status;
 #define OK 0
-#define FALSE 0
 #define ERROR -1
 #define OVERFLOW -2
 #define maxsize 10
@@ -24,7 +23,7 @@ Status Init(sqlist &l)
 
 Status Writeelem(sqlist &l ,int i){
     if (i<1||i>maxsize)return OVERFLOW;
-    for (int j = 1; j < maxsize+2; ++j) {
+    for (int j = 1; j < maxsize+1; ++j) {
         l.elem[j] = j;
         l.length++;
     }
@@ -35,24 +34,36 @@ Status Getelem(sqlist l,int i,Element &e)
 {
     if (i<1||i>l.length) return ERROR;
     e = l.elem[i];
-    for (int j = 0; j < l.length; ++j) {
-        if(l.elem[j]==e)
-            return j;
-    }
     return OK;
 }
 
 Status Insertelem(sqlist &l,int i,Element &e)
 {
+    if(i>l.length||!l.elem||l.length==10)
+        return ERROR;
+    for (int j = l.length; j >= i; j--) {
+        l.elem[j+1]=l.elem[j];
+    }
+    return OK;
+}
+
+Status Deletelist(sqlist &l,int i,Element &e)
+{
+    if(!l.elem||i>l.length||i<1)return ERROR;
+    e = l.elem[i];
+    for (int j = i; j < l.length; ++j) {
+        l.elem[j]=l.elem[j+1];
+    }
     return OK;
 }
 
 int main(){
     int e;
-    auto *l= new sqlist ();
-    Init(*l);
-    Writeelem(*l,10);
-    cout<<l->length;
-    cout<<Getelem(*l,9,e);
+    sqlist l;
+    Init(l);
+    Writeelem(l,10);
+    cout<<l.length<<endl;
+    Getelem(l,9,e);
+    cout<<e;
     return 0;
 }
